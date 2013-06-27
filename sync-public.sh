469db698fa1c08e7bc904b206daeeb1d0039789f
Mark Browning <mabrowningrr@gmail.com>
2013-06-26 19:31:09 -0500

    sync-public.sh now hides private repo URL

--- sync-public.sh
+++ sync-public.sh
@@ -10,7 +10,7 @@ export GIT_DIR=$WORK_TREE/.git
 git rev-list public..master | tac | while read REV; do
 	AUTHOR=$(git log --pretty=format:"%an <%ae>" -1 $REV)
 	DATE=$(git log --pretty=format:"%ad" -1 $REV)
-	BODY=$(git log --pretty=format:"%B" -1 $REV)
+	BODY=$(git log --pretty=format:"%B" -1 $REV | sed '/Merge/s/of http[^ \t\n\r]*/of private-repo/' )
 	GIT_WORK_TREE=$WORK_TREE git checkout $REV
 	./getchanges.sh
 	GIT_DIR=.git git commit --author="$AUTHOR" --date="$DATE" -m "$BODY" patches src
