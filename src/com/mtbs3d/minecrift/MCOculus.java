8cdd35def4d56e79a1b7cf77afd175e72065c46d
StellaArtois <pete.tabram@gmail.com>
2013-07-03 07:18:44 +0100

    Improve progress indicators during Oculus calibration

--- src/com/mtbs3d/minecrift/MCOculus.java
+++ src/com/mtbs3d/minecrift/MCOculus.java
@@ -28,6 +28,7 @@ public class MCOculus extends OculusRift //OculusRift does most of the heavy lif
     private long coolDownStart = 0L;
     private long lastUpdateAt = 0L;
     private int calibrationStep = NOT_CALIBRATING;
+    private int MagCalSampleCount = 0;
 
 	@Override
 	public String getName() {
@@ -82,7 +83,21 @@ public class MCOculus extends OculusRift //OculusRift does most of the heavy lif
             case CALIBRATE_AWAITING_MAG_CAL:
             case CALIBRATE_MAG_CAL_WAIT:
             {
-                step = "Slowly look left, right, up";
+                switch (MagCalSampleCount)
+                {
+                    case 0:
+                        step = String.format("Look forward (%d/4)", new Object[] {Integer.valueOf(MagCalSampleCount)});
+                        break;
+                    case 1:
+                        step = String.format("Look all the way left (%d/4)", new Object[] {Integer.valueOf(MagCalSampleCount)});
+                        break;
+                    case 2:
+                        step = String.format("Look all the way right (%d/4)", new Object[] {Integer.valueOf(MagCalSampleCount)});
+                        break;
+                    default:
+                        step = String.format("Look all the way up (%d/4)", new Object[] {Integer.valueOf(MagCalSampleCount)});
+                        break;
+                }
                 break;
             }
             case CALIBRATE_COOLDOWN:
@@ -151,12 +166,13 @@ public class MCOculus extends OculusRift //OculusRift does most of the heavy lif
             {
                 _reset();
                 _beginAutomaticCalibration();
+                MagCalSampleCount = 0;
                 calibrationStep = CALIBRATE_AWAITING_MAG_CAL;
                 break;
             }
             case CALIBRATE_AWAITING_MAG_CAL:
             {
-                _updateAutomaticCalibration();
+                MagCalSampleCount = _updateAutomaticCalibration();
                 if (_isCalibrated())
                 {
                     lastUpdateAt = 0;
