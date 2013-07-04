75f6eced9f7743cdc7ab8dc57c627adcd8e50826
Mark Browning <mabrowningrr@gmail.com>
2013-07-04 00:48:33 -0500

    Changing BUILD to BUILD_NUMBER for jenkins

--- build.py
+++ build.py
@@ -73,7 +73,7 @@ def main(mcp_dir):
     commands.creatergcfg(reobf=True, keep_lvt=True, keep_generics=True, srg_names=False)
     reobfuscate_side( commands, CLIENT )
 
-    version = os.getenv("BUILD") or "1.1_beta_pre"
+    version = os.getenv("BUILD_NUMBER") or "1.1_beta_pre"
 
     try:
         os.mkdir( os.path.join( base_dir, "releases" ) )
